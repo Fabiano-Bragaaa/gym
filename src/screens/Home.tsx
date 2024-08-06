052bf86d02efdcd8ceee06b0dc507b56568dfbee
Fabiano Braga <fabianobragadev@gmail.com>
2024-08-06 18:29:51 -0300

    update today

---
+++ src/screens/Home.tsx
@@ -0,0 +1,41 @@
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { HStack, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+
+import { FlatList } from "react-native";
+
+export function Home() {
+  const [groups, setGroups] = useState<string[]>([
+    "Costas",
+    "Biceps",
+    "Triceps",
+    "Ombro",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{
+          marginVertical: 40,
+          maxHeight: 44,
+          minHeight: 44,
+        }}
+      />
+    </VStack>
+  );
+}
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,74 @@
+import {
+  Center,
+  Heading,
+  Image,
+  Text,
+  VStack,
+  ScrollView,
+} from "@gluestack-ui/themed";
+
+import BackgroundImage from "@assets/background.png";
+
+import Logo from "@assets/logo.svg";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useNavigation } from "@react-navigation/native";
+
+export function SignUp() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleSignIn() {
+    navigation.navigate("signIn");
+  }
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1}>
+        <Image
+          source={BackgroundImage}
+          alt="Pessoas treinando"
+          w="$full"
+          h={624}
+          defaultSource={BackgroundImage}
+          position="absolute"
+        />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
+
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua meente e o seu corpo
+            </Text>
+          </Center>
+
+          <Center gap="$2" flex={1}>
+            <Heading color="$gray100">Crie sua conta</Heading>
+
+            <Input placeholder="Nome" />
+
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Senha" secureTextEntry />
+
+            <Button title="Criar e acessar" />
+          </Center>
+
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt="$12"
+            onPress={handleSignIn}
+          />
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+}
