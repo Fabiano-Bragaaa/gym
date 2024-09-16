c2371c232c771b9d4e9902ce898f73c242266e3f
Fabiano Braga <fabianobragadev@gmail.com>
2024-09-16 18:45:19 -0300

    screens done

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,98 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import {
+  Box,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Text,
+  VStack,
+} from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
+
+import { ArrowLeft } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import BodySvg from "@assets/body.svg";
+import SeriesSvg from "@assets/series.svg";
+import RepetitionSvg from "@assets/repetitions.svg";
+import { Button } from "@components/Button";
 
 export function Exercise() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$12">
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt="$4"
+          mb="$8"
+        >
+          <TouchableOpacity onPress={handleGoBack}>
+            <Icon as={ArrowLeft} color="$green500" size="xl" />
+          </TouchableOpacity>
+          <Heading
+            color="$gray100"
+            fontFamily="$heading"
+            fontSize="$lg"
+            flexShrink={1}
+          >
+            Puxada Frontal
+          </Heading>
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="$gray200" ml="$1" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 32 }}
+      >
+        <VStack p="$8">
+          <Image
+            source={{
+              uri: "https://lh3.googleusercontent.com/proxy/qNO1_IYsJYMm2oe4kFH16HNESQsCFm_ZrxxbPNccqxXROxlYXVfhIrODqsPcqiVy3g6wXXU4O0Y",
+            }}
+            alt="exercicio"
+            mb="$3"
+            resizeMode="cover"
+            rounded="$lg"
+            w="$full"
+            h="$80"
+          />
+          <Box bg="$gray600" rounded="$md" pb="$4" px="$4">
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb="$6"
+              mt="$5"
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="$gray200" ml="$2">
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionSvg />
+                <Text color="$gray200" ml="$2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
